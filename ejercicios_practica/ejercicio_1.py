36a9b96a43dc524538c13a9f62d47ab19b198cec
sebacame1412 <105613853+sebacame1412@users.noreply.github.com>
2022-06-12 23:58:54 -0300

    Ejercicio N°1 terminado

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -10,7 +10,13 @@
 # Ejercicios con funciones
 
 def imprimir_mayor(numero_1, numero_2):
-    print("Funcion imprimir mayor")
+    if numero_1 > numero_2:
+      return numero_1
+
+    else:
+        return numero_2
+
+print("Funcion imprimir mayor")        
     # En esta función debe determinar cual de los dos
     # números ingresados por parámetro es mayor
     # y luego imprimir dicho valor en pantalla
@@ -20,6 +26,8 @@ if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
     
     # Alumno: Complete la función "imprimir_mayor"
-    imprimir_mayor(2, 10)
+    mayor= imprimir_mayor(2, 10)
+
+    print("El numero mayor es:", mayor)
 
     print("terminamos")
